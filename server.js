2d0ed3a75ebe44326f7bd66f5fa9c0291738ffab
naidu283 <59912667+naidu283@users.noreply.github.com>
2020-03-05 21:50:19 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const router = require('./api/route/index');
+
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
